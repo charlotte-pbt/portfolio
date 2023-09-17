ac045dfe984a28115937acbb0fa19ee9db9e779e
Charlotte Philibert <charlotte.philibert@epita.fr>
2023-09-17 17:00:57 +0200

    Updates

---
+++ _app/immutable/nodes/3.db2e3df9.js
@@ -0,0 +1,7 @@
+import{s as c,n as t}from"../chunks/scheduler.b108d059.js";import{S as r,i as d,s as h,g as m,B as p,f as s,c as v,h as _,x as u,k as f,a as i}from"../chunks/index.26299741.js";function S(o){let e,a,n=`<section><div class="inner-container"><h2 class="about-title svelte-63zj48">I&#39;m Charlotte Philibert 👋</h2> <div class="about-subtitle svelte-63zj48"><p class="svelte-63zj48">Sblab lablablabla blabalbal balabal bal
+               Sblab lablablablablabal balb alabalbal
+               Sblablablabl ablablabalbalbalabalbal
+               Sb lablablablablablabalbalbalabalbal
+               Sblablablablablablabal  balbalabalbal
+               Sblablabl ablablablabalbalbalabalbal
+               Sblablablablablablaba lbalbalabalbal</p> <p class="svelte-63zj48">You can send me an email at <span class="info-text svelte-63zj48">charlotte56.philibert@gmail.com</span></p></div></div></section>`;return{c(){e=h(),a=m("div"),a.innerHTML=n,this.h()},l(l){p("svelte-obu56y",document.head).forEach(s),e=v(l),a=_(l,"DIV",{class:!0,"data-svelte-h":!0}),u(a)!=="svelte-11s6x94"&&(a.innerHTML=n),this.h()},h(){document.title="About",f(a,"class","main_container svelte-63zj48")},m(l,b){i(l,e,b),i(l,a,b)},p:t,i:t,o:t,d(l){l&&(s(e),s(a))}}}class j extends r{constructor(e){super(),d(this,e,null,S,c,{})}}export{j as component};

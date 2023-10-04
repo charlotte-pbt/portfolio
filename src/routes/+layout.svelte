e45735c2f030f62f4f63f612aa111da9de00a281
Charlotte Philibert <charlotte.philibert@epita.fr>
2023-10-05 00:52:23 +0200

    add favicon visible on all divice (except safari on mobile)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,6 @@
     import { base } from '$app/paths';
 </script>
 
-<svelte:head>
-  <link rel="icon" type="image/svg" href="https://raw.githubusercontent.com/charlotte-pbt/portfolio/master/src/images/icon.png" />
-</svelte:head>
-
 <nav class="header">
     <div class="nav-container">
         <a href="{base}/" class="nav-logo" title="Back to Homepage">Charlotte Philibert</a>

f114b86bbef51ef410be17e4e918cb73b6a250e2
Charlotte Philibert <charlotte@mbpdecharlotte.home>
2023-10-24 22:19:24 +0200

    resolve resume problem on safari

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -4,52 +4,27 @@
 </svelte:head>
 
 <script>
-    import { onMount } from "svelte";
     let url = "https://files.tanguymaraux.com/api/public/dl/PtB4__r7?inline=true"
-
-    onMount(() => {
-        // Obtenez la hauteur de la nav-container
-        let navContainer = document.querySelector('.header');
-        let navContainerHeight = 0;
-        if (navContainer instanceof HTMLElement) {
-            navContainerHeight = navContainer.offsetHeight;
-
-        // Appliquez la hauteur calculée à la div
-        const maDiv = document.querySelector('.all');
-
-        if (maDiv instanceof HTMLElement) {
-            maDiv.style.height = `calc(100vh - ${navContainerHeight}px)`;
-        }
-        }
-    });
 </script>
 
 <div class="all">
 
-    <iframe
-        class="responsive-iframe"
+    <embed
         title="CV_Charlotte_Philibert"
         src={url}
-        frameborder="0"
-    ></iframe>
+        type="application/pdf"
+        width="100%"
+        height="1000"
+    />
 
 </div>
 
 <style>
 
 .all {
-    position: relative;
     margin: 0 70px 0 70px;
 }
 
-.responsive-iframe {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border: 0; 
-}
 
 @media screen and (max-width: 768px) {
 		.all {
